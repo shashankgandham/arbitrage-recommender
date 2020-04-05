9b73a7fcd3e6eeaf11862a31793672cd0952866e
Sum1999 <51736573+Sum1999@users.noreply.github.com>
2020-04-05 23:42:15 +0530

    Add files via upload
    This is the basic Spring Boot Application code

---
+++ AR-Services/src/main/java/com/citibridge/arbitragerecommendation/config/ArBackendApplication.java
@@ -0,0 +1,13 @@
+package com.citibridge.arbitragerecommendation.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArBackendApplication.class, args);
+	}
+
+}
---
+++ AR-Services/src/test/java/com/citibridge/arbitragerecommendation/config/ArBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citibridge.arbitragerecommendation.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
